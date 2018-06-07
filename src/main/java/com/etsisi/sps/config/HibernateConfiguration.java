4fd7d14ac04d6dd20804b230bb8c9e31fccd397e
Emilio Castellano <ecastel6@gmail.com>
2018-06-07 19:57:43 +0200

    hibernate addons

--- src/main/java/com/etsisi/sps/config/HibernateConfiguration.java
+++ src/main/java/com/etsisi/sps/config/HibernateConfiguration.java
@@ -25,7 +25,7 @@ public class HibernateConfiguration {
     private Environment environment;
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
+    public  LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan(new String[] { "com.etsisi.sps.model" });
@@ -48,6 +48,8 @@ public class HibernateConfiguration {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.hbm2ddl.import_files","/sql/SpringDML.sql");
         return properties;        
     }
     
